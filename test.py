85c165ae71b5c6d97bd86be7612710c5d2af7793
Quentin2308 <115085012+Quentin2308@users.noreply.github.com>
2022-10-05 16:21:54 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 from periphery import Serial
 test_string = "Je teste le port série 1 2 3 4 5"
 byte = bytes (test_string, 'utf-8')
-port = "/dev/ttyS0"
+port = "/dev/ttyS1"
 
 serialPort = Serial(port, 9600)
 print ("Port Série ", port, " ouvert pour le test :")
