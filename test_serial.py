c869aadea842fbd4447ef7fd0620489e524230e4
Quentin2308 <115085012+Quentin2308@users.noreply.github.com>
2022-10-05 15:34:54 +0200

    Update test_serial.py

--- test_serial.py
+++ test_serial.py
@@ -6,7 +6,7 @@ serial = Serial("/dev/ttyS0", 9600)
 serial.write(b"Hello World!")
 
 # Read up to 128 bytes with 500ms timeout
-buf = serial.read(128, 0.5)
+buf = serial.read(100, 2)
 print("read {:d} bytes: {:s}".format(len(buf), buf.decode('utf-8')))
 
 serial.close()
