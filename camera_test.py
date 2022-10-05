41b74704359e93e6d87b286c2ce576352d0d8962
Quentin2308 <115085012+Quentin2308@users.noreply.github.com>
2022-10-05 17:04:42 +0200

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -1,1 +1,15 @@
 #test for the camera connection
+from periphery import Serial
+#test_string = "Je teste le port série 1 2 3 4 5"
+#byte = bytes (test_string, 'utf-8')
+port = "/dev/ttyS1"
+
+serialPort = Serial(port, 9600)
+print ("Port Série ", port, " ouvert pour le test :")
+#bytes_sent = serialPort.write(byte)
+
+loopback = serialPort.read(bytes_sent, timeout = 0.5)
+
+print (loopback.decode('utf-8'))
+
+serialPort.close()
