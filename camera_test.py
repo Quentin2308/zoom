69e6a47e98f47653d157a3c2fcce8ce6902716b6
Quentin2308 <115085012+Quentin2308@users.noreply.github.com>
2022-10-06 11:30:50 +0200

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -7,12 +7,13 @@ port = "/dev/ttyS1"
 
 serialPort = Serial(port, 9600)
 print ("Port Série ", port, " ouvert pour le test :")
+sequenceNumber = 1
 #bytes_sent = serialPort.write(byte)
 # for general commands (payload type 0100), command should be bytes
 command = bytearray.fromhex("8101040003ff")
 print(command)
 length = len(command).to_bytes(2, 'big')
-command = b"\x01\x00" + length + self.sequenceNumber.to_bytes(4, 'big') + command
+command = b"\x01\x00" + length + sequenceNumber.to_bytes(4, 'big') + command
 print(command)
 serialPort.write(command)
 #data = sendRawCommand(self.ip, command, skipCompletion=skipCompletion) # TODO: deal with udp packets getting lost and sequence number desyncing (see manual)
