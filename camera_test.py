aa32eaa31c6e286c7a29d924e9d4e9d9792bc4f0
Quentin2308 <115085012+Quentin2308@users.noreply.github.com>
2022-10-21 12:26:08 +0200

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -5,10 +5,18 @@ from time import sleep
 port = "/dev/ttyS1"
 
 inquiry = bytearray.fromhex("883001FF")
+toto3 = inquiry[0:23]
+print (toto3)
+toto4 = int.from_bytes(toto3, byteorder="big") 
+#print (toto4)
+toto5 = bin(toto4)[2::]
+#print (toto5)
+#print (len(toto5))
+toto6 = ba.bitarray(toto5)
 
 serialPort = Serial(port, 9600, databits=8, stopbits=1)
 print ("Port Série ", port, " ouvert pour le test :")
-sequenceNumber = 1
+#sequenceNumber = 1
 
 print(inquiry)
 
@@ -16,14 +24,14 @@ bytes_sent = serialPort.write(inquiry)
 # for general commands (payload type 0100), command should be bytes
 #command = bytearray.fromhex("8101040003ff")
 
-length = len(inquiry).to_bytes(2, 'big')
-print ("longueur = ", length)
+#length = len(inquiry).to_bytes(2, 'big')
+#print ("longueur = ", length)
 
-command = b"\x01\x00" + length + sequenceNumber.to_bytes(4, 'big') + inquiry
-print(command)
+#command = b"\x01\x00" + length + sequenceNumber.to_bytes(4, 'big') + inquiry
+#print(command)
 
 serialPort.write(command)
-sequenceNumber += 1
+#sequenceNumber += 1
 
 #data = sendRawCommand(self.ip, command, skipCompletion=skipCompletion) # TODO: deal with udp packets getting lost and sequence number desyncing (see manual)
 #PowerOff = bytearray.fromhex("8101040003ff")
